b52cb347f2eaad561eb2f39c57df20d27a94900f
Aaron Hicks (XPS) <hicksa@landcareresearch.co.nz>
2012-10-10 16:51:12 +1300

    Tweaks to .gitignore and Moduelfile.
    Add parameters to class that represent the oabjava script command line options.

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,9 @@
 # This module manages oabjava
 #
 # Parameters:
+# 		java7 			- Build Java 7 packages instead of Java 6 (the default)
+#		clean_old_pkgs	- Clean out previously existing Java packages from the apt cache
+#		skip_if_built	- Skip building packages if they already exist
 #
 # Actions:
 #
@@ -12,7 +15,11 @@
 #
 # This file is part of the oabjava Puppet module.
 # [Remember: No empty lines between comments and class definition]
-class oabjava {
+class oabjava (
+	$java7			= false,
+	$clean_old_pkgs	= false,
+	$skip_if_built	= true
+){
 
 
 }
